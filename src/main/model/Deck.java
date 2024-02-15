01371180ccb21db77b65e2424743e7c80da5736d
Dustin Yochim <yochim.dustin@gmail.com>
2024-02-15 16:44:54 -0600

    feat: Implement Deck and Hand classes
    This commit introduces the Deck class responsible for managing the deck of playing cards and the Hand class to represent a player's hand in the game.

--- src/main/model/Deck.java
+++ src/main/model/Deck.java
@@ -1,4 +1,34 @@
 package main.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Deck {
+
+    private final List<Card> cards;
+
+    public Deck() {
+        cards = new ArrayList<>();
+        initializeDeck();
+    }
+
+    private void initializeDeck() {
+        for (Suit suit : Suit.values()) {
+            for (Rank rank : Rank.values()) {
+                cards.add(new Card(rank, suit));
+            }
+        }
+    }
+
+    public void shuffleDeck() {
+        Collections.shuffle(cards);
+    }
+
+    public void dealCardsIntoHand(Hand hand, int numCards) {
+        this.shuffleDeck();
+        for (int i = 0; i < numCards; i++) {
+            hand.addCard(cards.get(i));
+        }
+    }
 }
--- src/main/model/Hand.java
+++ src/main/model/Hand.java
@@ -1,4 +1,31 @@
 package main.model;
 
+import java.util.ArrayList;
+
 public class Hand {
+    private final ArrayList<Card> cards;
+
+    public Hand() {
+        cards = new ArrayList<>();
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public ArrayList<Card> getHand() {
+        return cards;
+    }
+
+    public String format_hand_for_logger() {
+        StringBuilder sb = new StringBuilder();
+        for (Card card : cards) {
+            sb.append(card.format_card_for_logger()).append(",");
+        }
+        if (!sb.isEmpty()) {
+            // Remove trailing comma
+            sb.setLength(sb.length() - 1);
+        }
+        return sb.toString();
+    }
 }
